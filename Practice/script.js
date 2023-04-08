73b92b0278205359ee06e7753b7ffbc53dfd9707
Aman Singh <kramans763@gmail.com>
2023-04-08 13:44:31 +0530

    js

--- Practice/script.js
+++ Practice/script.js
@@ -1,1 +1,34 @@
 console.log("Hellow world");
+console.log(1+3);
+
+//------Variable-------
+// old js
+//-------Before ES6-------
+// var name="Aman";
+// var age=24;
+// var canVote=true;
+
+//console.log(name , age, canVote);
+// console.log(age);
+// console.log(canVote);
+
+//---------------------------------------
+
+// in 2022 .. var won't be used  .. two new keyword --> let and const
+
+//variable (it can be reassign)
+// let name="aman";
+// let age=24;
+// let canVote=true;
+// let gender="M";
+
+// age=26;
+
+// console.log(name, age,canVote,gender);
+
+//const (it cannot reassign)
+
+const name="aman";
+name="amasd";
+console.log(name);
+
