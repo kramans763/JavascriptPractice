2609615c24233fd96ee79325977e337a98bc35eb
Aman Singh <kramans763@gmail.com>
2023-04-08 11:57:11 +0530

    initial commit

---
+++ Practice/scipt.js
@@ -0,0 +1,9 @@
+console.log("aman");
+
+echo "# JavascriptPractice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kramans763/JavascriptPractice.git
+git push -u origin main
