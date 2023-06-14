e72686c706484dd9091be49b827544551c9798fd
Aman Singh <kramans763@gmail.com>
2023-06-14 18:36:36 +0530

    updated

--- Practice/script.js
+++ Practice/script.js
@@ -1,5 +1,5 @@
-console.log("Hellow world");
-console.log(1+3);
+// console.log("Hellow world");
+// console.log(1+3);
 
 //------Variable-------
 // old js
@@ -28,7 +28,85 @@ console.log(1+3);
 
 //const (it cannot reassign)
 
-const name="aman";
-name="amasd";
-console.log(name);
+//const name="aman";
+//name="amasd";  can't do
+//console.log(name);
+
+// let car= {
+//     wheels:"yes",
+//     seats : 4,
+//     powerbreak : true,
+
+// }
+// console.log(car.seats , car.wheels);
+
+//-----------------------------------------------------------------------
+
+// -------------------------Array:----------------------------------------
+
+//Array can take any type of value in javascript
+// let arr =[1,2,3,5,"aman" , true, 4];
+// console.log(arr);
+// console.log(arr[1]);
+// console.log(arr.length , "is length of array");
+
+// //weired thing
+
+// arr.length=0; //this is not good
+// console.log(arr ,"this is updated array");
+
+// ----Array Properties-----------
+ 
+   // 1. push
+
+//    let arr=[1,2,3,4];
+//    arr.push(5);
+//    console.log(arr);
+   
+//    // 2.pop
+//        arr.pop(); // remove last element from array
+//        console.log(arr); 
+
+//   // shift
+//      arr.shift(); //Removes the first element from an array 
+//      console.log(arr);
+
+
+ //-------------Hoisting-------------
+ 
+ // variable and function DECLARATION are hoisted in Javascript
+ 
+// let a=23;
+// console.log(a);
+
+//when variables
+
+// console.log(a);
+// console.log(b);
+// var a=12;  // print undefined   but in java it gives error
+// let b=23;  // it gives error bcs hoisting will not work with let 
+
+// // but when function
+// sum()
+// function sum(){
+//     console.log("inside function");
+// }
+
+//-----------Basis Math (assignement , operators)--------------
+
+console.log(1+2);           //3
+console.log(1>2);           //false
+console.log(10%2);          //0
+console.log(Math.pow(2,3)); // 8
+console.log(2 ** 3);        //8
+
+// convert string to int using math
+let val= +"123"+2;
+console.log(val);            // 125
+
+       //--------Comparision----------
+
+   // < , > , <= , >= , == , != , === , !==
+   
+   
 
